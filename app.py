77ade9a268ae0acef064c9f4e78003a85bacfc24
Hadi Zorkot <hadizorkot.hadi@gmail.com>
2021-06-06 16:30:48 +0300

    First deployment

---
+++ app.py
@@ -0,0 +1,14 @@
+from flask import Flask, render_template, request
+import pytube
+
+app = Flask(__name__)
+
+@app.route("/", methods=['GET', 'POST'])
+def index():
+    if request.method == "GET":
+        return render_template('index.html')
+    else:
+        link = request.form.get("video-link")
+        yt = pytube.YouTube(link)
+        yt.streams.filter(progressive=True).first().download(max_retries=5)
+        return render_template('index.html')
