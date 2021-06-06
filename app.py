a71f0fbfd0c643327da8061bd2e3f3396b47f2be
Hadi Zorkot <hadizorkot.hadi@gmail.com>
2021-06-06 19:01:49 +0300

    fixed download location

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, request
 import pytube
+import os
 
 app = Flask(__name__)
 
@@ -8,7 +9,8 @@ def index():
     if request.method == "GET":
         return render_template('index.html')
     else:
+        home = os.path.expanduser("~")
         link = request.form.get("video-link")
         yt = pytube.YouTube(link)
-        yt.streams.filter(progressive=True).first().download('~/Downloads')
+        yt.streams.filter(progressive=True).first().download(os.path.join(home, "Downloads"))
         return render_template('index.html')
